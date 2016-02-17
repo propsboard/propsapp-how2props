9fb337ed177f0bdc97acba5cbe6007bcfe83fedf
Justin Furniss <primeobsession@gmail.com>
2016-02-17 18:11:27 -0500

    Little clean up. The first commit has never, ever, been the final commit.  Let this be a lesson to all of you

--- js/app.js
+++ js/app.js
@@ -2,39 +2,25 @@
 
 
 var app = {
-    AppLoad: function(data, app){
+  AppLoad: function (data, app) {
 
+    var slidesToShow = [
+      './img/cards/how_to_props_message.png',
+      './img/cards/how_to_shoutout.png',
+      './img/cards/how_to_props_image.png',
+    ];
 
-
-      var slidesToShow =[
-        './img/cards/how_to_props_message.png',
-        './img/cards/how_to_shoutout.png',
-        './img/cards/how_to_props_image.png',
-      ];
-
-      for(var i =0; i<slidesToShow.length;i++){
-        $('.sliderList')
-          .append($('<li>')
-            .css('background-image', 'url(' + slidesToShow[i] + ')'));
-      }
-
-
-
-      setTimeout(function(){
-        $('.how2slider').unslider({
-          autoplay: true,
-          delay: 18000,
-          nav: false,
-          arrows: false
-        });
-      }, 100);
+    for (var i = 0; i < slidesToShow.length; i++) {
+      $('.sliderList')
+        .append($('<li>')
+          .css('background-image', 'url(' + slidesToShow[i] + ')'));
     }
-  };
-jQuery(document).load(function($) {
 
-
-
-
-
-
-});
+    $('.how2slider').unslider({
+      autoplay: true,
+      delay: 18000,
+      nav: false,
+      arrows: false
+    });
+  }
+}
